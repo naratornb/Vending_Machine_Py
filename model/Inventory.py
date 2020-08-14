b2a1a291e96bf7738d4a7772c3aa52f5f7b8d5bb
Narartorn <nar@gmail.com>
2020-08-14 16:50:01 +0700

    Add model

---
+++ model/Inventory.py
@@ -0,0 +1,9 @@
+class Inventory:
+    def __init__(self,noCokeStock, noPepsiStock, noSodaStock, noQuarter, noDime, noNickel, noPenny):
+        self.noCokeStock = noCokeStock
+        self.noPepsiStock = noPepsiStock
+        self.noSodaStock = noSodaStock
+        self.noQuarter = noQuarter
+        self.noDime = noDime
+        self.noNickel = noNickel
+        self.noPenny = noPenny
